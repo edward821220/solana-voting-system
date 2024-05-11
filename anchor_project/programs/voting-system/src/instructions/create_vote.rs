e70a074d444585fab30b5d35c32dc15ba6b4ac63
Edward <edward821220@gmail.com>
2024-05-12 07:55:31 +0800

    feat: replace max size with init space

--- anchor_project/programs/voting-system/src/instructions/create_vote.rs
+++ anchor_project/programs/voting-system/src/instructions/create_vote.rs
@@ -13,7 +13,7 @@ pub fn _create_vote(ctx: Context<CreateVote>, topic: String, options: Vec<String
 #[derive(Accounts)]
 #[instruction(topic:String)]
 pub struct CreateVote<'info> {
-    #[account(init, payer = user, space = 8 + Vote::MAX_SIZE, seeds = [b"vote",topic.as_bytes()], bump)]
+    #[account(init, payer = user, space = 8 + Vote::INIT_SPACE, seeds = [b"vote",topic.as_bytes()], bump)]
     pub vote: Account<'info, Vote>,
     #[account(mut)]
     pub user: Signer<'info>,
--- anchor_project/programs/voting-system/src/state/vote.rs
+++ anchor_project/programs/voting-system/src/state/vote.rs
@@ -1,18 +1,12 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct Vote {
+    #[max_len(64)]
     pub topic: String,
+    #[max_len(20, 32)]
     pub options: Vec<String>,
+    #[max_len(20)]
     pub votes: Vec<u32>,
-}
-
-impl Vote {
-    pub const MAX_TOPIC_LENGTH: usize = 64;
-    pub const MAX_OPTION_LENGTH: usize = 32;
-    pub const MAX_OPTIONS_LENGTH: usize = 20;
-    pub const MAX_VOTES_LENGTH: usize = 20;
-    pub const MAX_SIZE: usize = (4 + Vote::MAX_TOPIC_LENGTH)
-        + (4 + Vote::MAX_OPTION_LENGTH * Vote::MAX_OPTIONS_LENGTH)
-        + (4 + 4 * Vote::MAX_VOTES_LENGTH);
 }
